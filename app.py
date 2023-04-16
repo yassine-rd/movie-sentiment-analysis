31320c0151a27e4365c4dc3e4ce8a2d990a8f819
yassine-rd <yassine.rodani@gmail.com>
2023-04-16 17:52:29 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,156 @@
+# -*-coding:Utf-8 -
+
+
+"""
+A simple machine learning application to predict the sentiment of a movie review.
+
+This script loads a trained sentiment classification model and uses it to predict
+the sentiment of a movie review entered by the user. The user interface is built using
+Streamlit, and includes a form to receive the review text and a button to trigger the
+prediction.
+
+The script uses text preprocessing techniques, such as removing stop words and lemmatizing
+words, to clean the input text before making the prediction. The cleaned text is then passed
+to the model, which predicts whether the sentiment of the review is positive or negative.
+The predicted sentiment and its probability are then displayed to the user.
+
+@Date: April 16th, 2023
+@Author: Yassine RODANI
+"""
+
+# import packages
+import streamlit as st
+import os
+import numpy as np
+
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from string import punctuation
+from nltk.tokenize import word_tokenize
+
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
+import re  # regular expression
+import joblib
+
+import warnings
+
+warnings.filterwarnings("ignore")
+# seeding
+np.random.seed(123)
+
+# load stop words
+stop_words = stopwords.words("english")
+
+# function to clean the text
+
+
+@st.cache_data
+def process_review(text, remove_stop_words=True, lemmatize_words=True, stemming_words=True) -> str:
+    """
+    Clean and preprocess text data by removing unwanted characters, stop words, and lemmatizing words.
+
+    This function takes a raw text input and processes it by removing non-alphanumeric characters,
+    punctuation, numbers, and optionally, stop words, lemmatizing and stemming. The output is a cleaned
+    and preprocessed version of the input text.
+
+    Parameters:
+    text (str): The raw text input to be processed.
+    remove_stop_words (bool, optional): If True, removes stop words from the input text. Defaults to True.
+    lemmatize_words (bool, optional): If True, lemmatizes words in the input text. Defaults to True.
+    lemmatize_words (bool, optional): If True, lemmatizes words in the input text. Defaults to True.
+
+    Returns:
+    str: The cleaned and preprocessed version of the input text.
+    """
+
+    # clean the text
+    text = re.sub(r"[^A-Za-z0-9]", " ", text)
+    text = re.sub(r"\'s", " ", text)
+    text = re.sub(r'https?://[^\s]+', '', text)  # remove hyperlinks
+    text = re.sub(r'\b\d+(?:\.\d+)?\s+', '', text)  # remove numbers
+
+    # remove punctuation from text
+    text = ''.join([c for c in text if c not in punctuation])
+
+    # remove stop words
+    if remove_stop_words:
+        text = text.split()
+        text = [w for w in text if not w in stop_words]
+        text = " ".join(text)
+
+    # lemmatize words
+    if lemmatize_words:
+        text = text.split()
+        lemmatizer = WordNetLemmatizer()
+        lemmatized_words = [lemmatizer.lemmatize(word) for word in text]
+        text = " ".join(lemmatized_words)
+
+    # shorten words to their stems
+    if stemming_words:
+        text = text.split()
+        stemmer = PorterStemmer()
+        stemmed_words = [stemmer.stem(word) for word in text]
+        text = " ".join(stemmed_words)
+
+    return text
+
+# functon to make prediction
+
+
+@st.cache_data
+def make_prediction(review)-> tuple:
+    """
+    Predicts the sentiment of a movie review using a trained classification model.
+
+    This function takes a movie review text as input, cleans the text using the
+    `process_review` function, loads a trained sentiment classification model from
+    a saved file, and uses the model to predict the sentiment of the review. The
+    predicted sentiment and its probability are returned as a tuple.
+
+    Parameters:
+        review (str): The movie review text to be classified.
+
+    Returns:
+        tuple: A tuple containing the predicted sentiment (1 for positive, 0 for negative)
+               and its probability (a float between 0 and 1).
+    """
+
+    # clearn the data
+    clean_review = process_review(review)
+
+    # load the model and make prediction
+    model = joblib.load("model/sentiment_model_pipeline.pkl")
+
+    # make prection
+    result = model.predict([clean_review])
+
+    # check probabilities
+    probas = model.predict_proba([clean_review])
+    probability = "{:.2f}".format(float(probas[:, result]))
+
+    return result, probability
+
+
+# Set the app title
+st.title("Movie Sentiment Analysis")
+st.write(
+    "A simple machine learning application to predict the sentiment of a movie's review"
+)
+
+# Include a form to receive a movie's review
+form = st.form(key="my_form")
+review = form.text_input(label="Please enter the text of your movie's review")
+submit = form.form_submit_button(label="Make Prediction")
+
+if submit:
+    # make prediction from the input text
+    result, probability = make_prediction(review)
+
+    # Display results of the NLP task
+    st.header("Results")
+
+    if int(result) == 1:
+        st.write("This is a positive review with a probabiliy of ", probability)
+    else:
+        st.write("This is a negative review with a probabiliy of ", probability)
